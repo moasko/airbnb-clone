22b36fe4cf058739e3b01699e114772aca110255
moasko.dev <42101671+moasko@users.noreply.github.com>
2022-12-30 04:00:47 +0000

    Update appConfig.js

--- config/appConfig.js
+++ config/appConfig.js
@@ -1,4 +1,4 @@
-const is_production = false;
+const is_production = true;
 
-export const BASE_URL =  is_production?'http://localhost:3000/':'http://localhost:3000/'
+export const BASE_URL =  is_production?'https://airbnb-clone-moasko.vercel.app/':'http://localhost:3000/'
 export const API_BASE_URL = `${BASE_URL}/api`
